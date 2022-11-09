ff99f58a4664cbd4ed5363de26a4187a381f0dd8
Arczg <apanczyk98@gmail.com>
2022-11-09 19:50:02 +0100

    add 3 activities

--- smb12/app/src/main/java/pl/panczyk/arkadiusz/smb1/MainActivity.kt
+++ smb12/app/src/main/java/pl/panczyk/arkadiusz/smb1/MainActivity.kt
@@ -1,11 +1,38 @@
 package pl.panczyk.arkadiusz.smb1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView: ListView = findViewById(R.id.listview)
+        val list: MutableList<String?> = ArrayList()
+        list.add("Products List")
+        list.add("Options")
+
+        val arrayAdapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, list)
+        listView.adapter = arrayAdapter
+        listView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
+            when (position) {
+                0 -> startActivity(Intent(this@MainActivity, OptionsActivity::class.java))
+                1 -> startActivity(Intent(this@MainActivity, ProductListActivity::class.java))
+                else -> throw Exception()
+            }
+        }
     }
+
+    // TODO
+    data class ViewHolder(
+        var thumbnail: ImageView? = null,
+        var title: TextView? = null,
+        var button: Button? = null,
+        var intent: Intent? = null
+    )
 }

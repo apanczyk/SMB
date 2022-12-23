a90cbdde54a2e971dab1726cc0248a18b0b1721d
Arczg <apanczyk98@gmail.com>
2022-12-23 01:13:06 +0100

    Change some files

--- smb51/app/src/main/java/pl/panczyk/arkadiusz/smb51/SmbWidget.kt
+++ smb51/app/src/main/java/pl/panczyk/arkadiusz/smb51/SmbWidget.kt
@@ -3,7 +3,6 @@ package pl.panczyk.arkadiusz.smb51
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -11,9 +10,7 @@ import android.util.Log
 import android.widget.RemoteViews
 import android.widget.Toast
 
-/**
- * Implementation of App Widget functionality.
- */
+
 class SmbWidget : AppWidgetProvider() {
 
     override fun onUpdate(
@@ -21,9 +18,8 @@ class SmbWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            createBrowser(context, appWidgetManager, appWidgetId)
         }
     }
 
@@ -43,15 +39,12 @@ class SmbWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
+internal fun createBrowser(
     context: Context,
     appWidgetManager: AppWidgetManager,
-    appWidgetId: Int,
+    appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    val views = RemoteViews(context.packageName, R.layout.smb_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
-
+    var views = RemoteViews(context.packageName, R.layout.smb_widget)
     val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
     val pendingIntent = PendingIntent.getActivity(
         context,
@@ -60,6 +53,7 @@ internal fun updateAppWidget(
         PendingIntent.FLAG_MUTABLE
     )
     views.setOnClickPendingIntent(R.id.browserBtn, pendingIntent)
-
+    views.setImageViewResource(R.id.imageView, R.drawable.example_appwidget_preview)
+    views.setImageViewResource(R.id.imageView2, R.drawable.example_appwidget_preview)
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
